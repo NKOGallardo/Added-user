6172e66d2536886544f18d389dd38ecbf5ba7107
NKOGallardo <nkogalado@gmail.com>
2026-09-13 21:16:33 +0200

    rename file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './Header/header'
-import Dropdown from './dropdown/dropdown'
+import Dropdown from './drop-down/dropdown'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
